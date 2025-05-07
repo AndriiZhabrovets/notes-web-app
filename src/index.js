28584970493c01a9788dcddb5de8be0df429103f
AndriiZhabrovets <zhabrovets.andrii@gmail.com>
2025-05-07 14:46:14 +0200

    Stuff

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
-const http = require('http');
-const lt = require('./latex-trs');
-const name = "result";
+//const http = require('http');
+//const lt = require('./latex-trs');
+//const name = "result";
+//
+////http.createServer(function (req, res) {
+////  res.writeHead(200, {'Content-Type': 'text/html'});
+////  res.end('Hello World!');
+////}).listen(8080); 
+//
+//
+//async function main() {
+//    lt.sHello();
+//    await lt.save(name);
+//    await lt.compile(name);
+//    await lt.clean(name);
+//
+//}
+//main()
 
-//http.createServer(function (req, res) {
-//  res.writeHead(200, {'Content-Type': 'text/html'});
-//  res.end('Hello World!');
-//}).listen(8080); 
 
+// Import Express
+const express = require('express');
+const app = express();
 
-async function main() {
-    lt.sHello();
-    await lt.save(name);
-    await lt.compile(name);
-    await lt.clean(name);
+// Define a route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express.js Tutorial');
+});
 
-}
-main()
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
